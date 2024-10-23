b2e88c898b1fcdfe038abdbb3f9ed5a16e53818f
burcuaydinn <burcuaydin2417@gmail.com>
2024-10-23 11:39:41 +0300

    Contact is done

--- NOVATREND/src/layout/PageContent.jsx
+++ NOVATREND/src/layout/PageContent.jsx
@@ -2,6 +2,7 @@ import { Switch, Route } from "react-router-dom";
 import HomePage from "../pages/HomePage";
 import ShopPage from "../pages/ShopPage";
 import ProductPage from "../pages/ProductPage";
+import ContactPage from "../pages/ContactPage";
 
 
 
@@ -17,6 +18,9 @@ const PageContent = () => {
         </Route>
         <Route path="/shop/product/:id">
           <ProductPage />
+        </Route>
+        <Route path="/contact">
+          <ContactPage />
         </Route>
       </Switch>
     </>
---
+++ NOVATREND/src/pages/ContactPage.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import  ContactHeader  from "../components/Contact/ContactHeader";
+import ContactContainer from "../components/Contact/ContactContainer";
+
+
+
+export default function ContactPage() {
+  return (
+    <div>
+      <ContactHeader />
+      <ContactContainer />
+    </div>
+  );
+}
